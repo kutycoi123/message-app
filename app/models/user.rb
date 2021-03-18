6c365fef8b6786684975bf7ef083550c6fa51804
kutycoi123 <kutycoi123@gmail.com>
2021-03-17 21:45:06 -0700

    Added message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maxmimum: 15 }
+  validates :username, presence: true, length: { minimum: 3, maxmimum: 15 },
+            uniqueness: { case_sensitive: false } 
+  has_many :messages, dependent: :destroy
   has_secure_password
 end
